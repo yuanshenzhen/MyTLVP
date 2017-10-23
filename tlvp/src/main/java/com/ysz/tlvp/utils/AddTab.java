f0bc171e528a1870780c4af3f80a7996b02b42d4
yuanshenzhen <358742662@qq.com>
2017-10-23 15:44:50 +0800

    Initial commit

---
+++ tlvp/src/main/java/com/ysz/tlvp/utils/AddTab.java
@@ -0,0 +1,41 @@
+package com.ysz.tlvp.utils;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/10/23.
+ */
+
+public class AddTab {
+    public static void addTab(TabLayout tabLayout, ViewPager viewPager, final List<Fragment> fragments,
+                              final List<String> titles, FragmentManager fragmentManager) {
+        /**
+         * 预加载
+         */
+        viewPager.setOffscreenPageLimit(fragments.size());
+        viewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return titles.get(position);
+            }
+        });
+        // TabLayout关联ViewPager
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
---
+++ tlvp/src/main/java/com/ysz/tlvp/utils/AddTitle.java
@@ -0,0 +1,16 @@
+package com.ysz.tlvp.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/10/23.
+ */
+
+public class AddTitle {
+    public static List<String> addTitle(String... s) {
+        String[] m = s;
+        List<String> list = Arrays.asList(m);
+        return list;
+    }
+}
